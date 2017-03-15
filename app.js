f5bc5005517496adda37f41e06c252077341776d
Chris <chris.gemske@gmail.com>
2017-03-15 19:41:29 -0400

    app.js initial

---
+++ app.js
@@ -0,0 +1,9 @@
+var comp1 = ["Desolate","Reactive","Morose","Infatuated","Creamy","Sullen","Limbic","Fortuitous","Meandering","Lousy"]
+var comp2 = ["Toaster","Poundcake","Horse","Dillweed","Member","Fallacy","Gargantuan","Tortoise","Firecracker","Paperclip"]
+var comp3 = Math.round(Math.random()*500);
+var randone = Math.round(Math.random()*9);
+var randtwo = Math.round(Math.random()*9);
+comp1 = comp1[randone];
+comp2 = comp2[randtwo];
+
+alert(comp1+comp2+comp3);
